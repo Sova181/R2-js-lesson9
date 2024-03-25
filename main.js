0bac47ec7300e8c57d5f1688d907c8b32474ba87
Таня Дашипилова <tanya.volk01@mail.ru>
2024-03-25 20:20:28 +0800

    4 коммит через терминал Lesson 9

--- main.js
+++ main.js
@@ -4,4 +4,10 @@ console.log( `1) kebab-case. Например: my-super-var.`);
 console.log( `2) snake_case. Например: my_super_var.`); 
 console.log( `3) CamelCase. Например: MySuperVar.`); 
 console.log( `4) lowerCamelCase . Например: mySuperVar. 
-В Javascript используется CamelCase и его вариация lowerCamelCase`); 
+В Javascript используется CamelCase и его вариация lowerCamelCase
+`); 
+
+console.log( "  Магические числа\n"); 
+console.log( `Числа, происхождение которых невозможно понять 
+без глубокого знания происходящего внутри данного участка кода.
+Поэтому нужно создавать переменные с правильными/понятными именами`); 
