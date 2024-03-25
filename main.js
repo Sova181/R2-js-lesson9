f7f0f130cf80655a1e97924272aef1f775cf8bcc
Таня Дашипилова <tanya.volk01@mail.ru>
2024-03-25 20:12:55 +0800

    2 коммит через терминал Lesson 9

--- main.js
+++ main.js
@@ -1,2 +1,4 @@
 console.log( "  Именование\n"); 
-console.log( "В именовании переменных есть 4 основных подхода:\n"); 
+console.log( `В именовании переменных есть 4 основных подхода:`); 
+console.log( `1) kebab-case. Например: my-super-var.`); 
+console.log( `2) snake_case. Например: my_super_var.`); 
