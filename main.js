1f48288eae753a143693851ae71ed6aec8621fce
Таня Дашипилова <tanya.volk01@mail.ru>
2024-03-25 20:16:28 +0800

    3 коммит через терминал Lesson 9

--- main.js
+++ main.js
@@ -2,3 +2,6 @@ console.log( "  Именование\n");
 console.log( `В именовании переменных есть 4 основных подхода:`); 
 console.log( `1) kebab-case. Например: my-super-var.`); 
 console.log( `2) snake_case. Например: my_super_var.`); 
+console.log( `3) CamelCase. Например: MySuperVar.`); 
+console.log( `4) lowerCamelCase . Например: mySuperVar. 
+В Javascript используется CamelCase и его вариация lowerCamelCase`); 
